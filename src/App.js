25a017bc27c08626e232a70f19bd421a0d0788f9
hansicaushi559 <75632107+hansicaushi559@users.noreply.github.com>
2022-04-06 20:07:54 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios';
+import search from './search.svg'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [wikiData, setWikiData] = useState([])
+  const [searchTerm, setSearchTerm] = useState ('')
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [error, setError] = useState(null);
+
+
+  const getData = async () => {
+    try {
+      const data = await axios.get('https://en.wikipedia.org/w/api.php' ,{
+        params: {
+          origin: '*',
+          action: 'opensearch',
+          search: {searchTerm}
+        }
+      });
+      setIsLoaded(true)
+      setWikiData(data.data[3].slice(0 , 5))
+    } catch (error) {
+      setIsLoaded(true)
+      setError(error)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
+  if (error) { return 
+    <div>
+      Error: {error.message}
+    </div> } else if (!isLoaded) {
+      return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="App" >
+        <input
+          type='text'
+          placeholder='Search'
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <img src={search}
+          onClick={() => setWikiData(searchTerm)} />
+        </div>
+        );
+  }
+
+
 }
 
 export default App;
